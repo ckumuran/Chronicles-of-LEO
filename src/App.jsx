d4ac916ba7f3c31a647423eb2aaa48cbbbfa53cb
kumaran <k31311558@gmail.com>
2026-05-16 15:56:13 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,54 @@
-import { Canvas } from '@react-three/fiber'
-import { Sky, PointerLockControls } from '@react-three/drei'
-import { Physics } from '@react-three/rapier'
-import World from './world/World'
-import Player from './player/Player'
-import Crosshair from './ui/Crosshair'
-import { worldBlocks } from "./world/worldData";
-export default function App() {
-  return (
-    <>
-      <Crosshair />
-      <Canvas
-        shadows
-        camera={{
-          position: [0, 10, 10],
-          fov: 75
-        }}
-      >
-        <Sky sunPosition={[100, 20, 100]} />
-        <ambientLight intensity={1.2} />
-        <directionalLight
-          castShadow
-          position={[20, 30, 20]}
-          intensity={2}
-          shadow-mapSize-width={2048}
-          shadow-mapSize-height={2048}
-        />
-        <Physics>
-          <World />
-         <Player blocks={worldBlocks} />
-        </Physics>
-        <PointerLockControls />
-      </Canvas>
-    </>
-  )
+import { Canvas,useThree } from "@react-three/fiber";
+
+import { useEffect } from "react";
+
+import World from "./world/World";
+
+import Player from "./player/Player";
+
+import Crosshair from "./ui/Crosshair";
+
+import { setupLighting }
+from "./engine/lighting/LightingEngine";
+
+function SceneSetup(){
+
+const { scene }=useThree();
+
+useEffect(()=>{
+
+setupLighting(scene);
+
+},[scene]);
+
+return null;
+
+}
+
+export default function App(){
+
+return(
+<>
+
+<Canvas
+shadows
+camera={{
+fov:75,
+position:[0,5,10]
+}}
+>
+
+<SceneSetup />
+
+<World />
+
+<Player />
+
+</Canvas>
+
+<Crosshair />
+
+</>
+);
+
 }

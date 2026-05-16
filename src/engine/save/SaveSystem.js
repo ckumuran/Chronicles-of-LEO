6aca5b39da8081f57530460f02119a873c7bcae6
kumaran <k31311558@gmail.com>
2026-05-16 15:56:42 +0530

    Update SaveSystem.js

--- src/engine/save/SaveSystem.js
+++ src/engine/save/SaveSystem.js
@@ -1,11 +1,42 @@
-export function saveWorld(blocks) {
-  localStorage.setItem(
-    "brixit_world",
-    JSON.stringify(blocks)
-  );
+export function saveChunk(
+chunk
+){
+
+const key=
+`chunk_${chunk.x}_${chunk.z}`;
+
+localStorage.setItem(
+key,
+JSON.stringify(chunk.blocks)
+);
+
 }
-export function loadWorld() {
-  return JSON.parse(
-    localStorage.getItem("brixit_world")
-  );
+
+export function loadChunk(
+x,
+z
+){
+
+const key=
+`chunk_${x}_${z}`;
+
+const data=
+localStorage.getItem(key);
+
+if(!data)return null;
+
+return JSON.parse(data);
+
+}
+
+export function saveWorld(
+chunks
+){
+
+chunks.forEach(chunk=>{
+
+saveChunk(chunk);
+
+});
+
 }

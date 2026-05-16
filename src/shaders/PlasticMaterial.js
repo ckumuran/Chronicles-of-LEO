95c846cece75a46bd1802ed102c32e0ccfac2c13
kumaran <k31311558@gmail.com>
2026-05-16 15:54:26 +0530

    Update PlasticMaterial.js

--- src/shaders/PlasticMaterial.js
+++ src/shaders/PlasticMaterial.js
@@ -1,7 +1,44 @@
 import * as THREE from "three";
-export const plasticMaterial =
-  new THREE.MeshStandardMaterial({
-    roughness: 0.3,
-    metalness: 0.05
 
-  });
+const materialCache={};
+
+export function getPlasticMaterial(
+color,
+transparent=false
+){
+
+const key=
+`${color}-${transparent}`;
+
+if(materialCache[key]){
+
+return materialCache[key];
+
+}
+
+const material=
+new THREE.MeshPhysicalMaterial({
+
+color,
+
+roughness:0.35,
+
+metalness:0.05,
+
+clearcoat:0.4,
+
+clearcoatRoughness:0.2,
+
+transparent,
+
+opacity:
+transparent?0.7:1
+
+});
+
+materialCache[key]=
+material;
+
+return material;
+
+}

bceebc343a93b7e7cb69132db962d3c888c2a3b9
kumaran <k31311558@gmail.com>
2026-05-16 15:10:44 +0530

    Create Hotbar.jsx

---
+++ src/inventory/Hotbar.jsx
@@ -0,0 +1,11 @@
+export default function Hotbar() {
+  return (
+    <div className="hotbar">
+      <div className="slot active"></div>
+      <div className="slot"></div>
+      <div className="slot"></div>
+      <div className="slot"></div>
+      <div className="slot"></div>
+    </div>
+  );
+}

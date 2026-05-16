150a75335ec98235a3e44d2afd49cfc010ab6da5
kumaran <k31311558@gmail.com>
2026-05-16 15:55:45 +0530

    Update LightingEngine.js

--- src/engine/lighting/LightingEngine.js
+++ src/engine/lighting/LightingEngine.js
@@ -1,21 +1,65 @@
 import * as THREE from "three";
-export function setupLighting(scene) {
-  const ambient =
-    new THREE.AmbientLight(
-      0xffffff,
-      0.8
-    );
-  scene.add(ambient);
-  const directional =
-    new THREE.DirectionalLight(
-      0xfff2cc,
-      1.5
-    );
-  directional.position.set(
-    20,
-    30,
-    20
-  );
-  directional.castShadow = true;
-  scene.add(directional);
+
+export function setupLighting(
+scene
+){
+
+const ambient=
+new THREE.AmbientLight(
+0xffffff,
+0.7
+);
+
+scene.add(ambient);
+
+const sun=
+new THREE.DirectionalLight(
+0xfff2cc,
+2
+);
+
+sun.position.set(
+50,
+80,
+30
+);
+
+sun.castShadow=true;
+
+sun.shadow.mapSize.width=
+2048;
+
+sun.shadow.mapSize.height=
+2048;
+
+sun.shadow.camera.near=1;
+
+sun.shadow.camera.far=200;
+
+sun.shadow.camera.left=-100;
+
+sun.shadow.camera.right=100;
+
+sun.shadow.camera.top=100;
+
+sun.shadow.camera.bottom=-100;
+
+scene.add(sun);
+
+const hemisphere=
+new THREE.HemisphereLight(
+0xb1e1ff,
+0x444422,
+0.5
+);
+
+scene.add(hemisphere);
+
+scene.fog=
+new THREE.Fog(
+"#b8dfff",
+40,
+180
+);
+
 }

5240d03551b9550e55bf75d97cfaa4d446e79673
kumaran <k31311558@gmail.com>
2026-05-20 16:17:36 +0530

    Update chunk_mesh.rs

--- src/engine/chunk_mesh.rs
+++ src/engine/chunk_mesh.rs
@@ -1,12 +1,15 @@
 use crate::engine::block::BlockType;
 use crate::engine::chunk::{Chunk, CHUNK_SIZE};
+use crate::engine::face_culling::FaceCulling;
 
 pub struct ChunkMesh {
     pub vertices: Vec<f32>,
 }
 
 impl ChunkMesh {
+
     pub fn build(chunk: &Chunk) -> Self {
+
         let mut vertices = Vec::new();
 
         for x in 0..CHUNK_SIZE {
@@ -20,12 +23,87 @@ impl ChunkMesh {
                         continue;
                     }
 
-                    Self::add_cube(
-                        &mut vertices,
-                        x as f32,
-                        y as f32,
-                        z as f32,
-                    );
+                    let x = x as f32;
+                    let y = y as f32;
+                    let z = z as f32;
+
+                    // FRONT
+                    if FaceCulling::is_face_visible(
+                        chunk,
+                        x as i32,
+                        y as i32,
+                        z as i32 + 1,
+                    ) {
+                        Self::add_front_face(
+                            &mut vertices,
+                            x,y,z
+                        );
+                    }
+
+                    // BACK
+                    if FaceCulling::is_face_visible(
+                        chunk,
+                        x as i32,
+                        y as i32,
+                        z as i32 - 1,
+                    ) {
+                        Self::add_back_face(
+                            &mut vertices,
+                            x,y,z
+                        );
+                    }
+
+                    // LEFT
+                    if FaceCulling::is_face_visible(
+                        chunk,
+                        x as i32 - 1,
+                        y as i32,
+                        z as i32,
+                    ) {
+                        Self::add_left_face(
+                            &mut vertices,
+                            x,y,z
+                        );
+                    }
+
+                    // RIGHT
+                    if FaceCulling::is_face_visible(
+                        chunk,
+                        x as i32 + 1,
+                        y as i32,
+                        z as i32,
+                    ) {
+                        Self::add_right_face(
+                            &mut vertices,
+                            x,y,z
+                        );
+                    }
+
+                    // TOP
+                    if FaceCulling::is_face_visible(
+                        chunk,
+                        x as i32,
+                        y as i32 + 1,
+                        z as i32,
+                    ) {
+                        Self::add_top_face(
+                            &mut vertices,
+                            x,y,z
+                        );
+                    }
+
+                    // BOTTOM
+                    if FaceCulling::is_face_visible(
+                        chunk,
+                        x as i32,
+                        y as i32 - 1,
+                        z as i32,
+                    ) {
+                        Self::add_bottom_face(
+                            &mut vertices,
+                            x,y,z
+                        );
+                    }
                 }
             }
         }
@@ -33,14 +111,13 @@ impl ChunkMesh {
         Self { vertices }
     }
 
-    fn add_cube(
+    fn add_front_face(
         vertices: &mut Vec<f32>,
         x: f32,
         y: f32,
         z: f32,
     ) {
-        let cube = [
-            // FRONT
+        let face = [
             x,y,z+1.0,
             x+1.0,y,z+1.0,
             x+1.0,y+1.0,z+1.0,
@@ -48,8 +125,18 @@ impl ChunkMesh {
             x+1.0,y+1.0,z+1.0,
             x,y+1.0,z+1.0,
             x,y,z+1.0,
+        ];
 
-            // BACK
+        vertices.extend_from_slice(&face);
+    }
+
+    fn add_back_face(
+        vertices: &mut Vec<f32>,
+        x: f32,
+        y: f32,
+        z: f32,
+    ) {
+        let face = [
             x,y,z,
             x,y+1.0,z,
             x+1.0,y+1.0,z,
@@ -57,8 +144,18 @@ impl ChunkMesh {
             x+1.0,y+1.0,z,
             x+1.0,y,z,
             x,y,z,
+        ];
 
-            // LEFT
+        vertices.extend_from_slice(&face);
+    }
+
+    fn add_left_face(
+        vertices: &mut Vec<f32>,
+        x: f32,
+        y: f32,
+        z: f32,
+    ) {
+        let face = [
             x,y+1.0,z+1.0,
             x,y+1.0,z,
             x,y,z,
@@ -66,8 +163,18 @@ impl ChunkMesh {
             x,y,z,
             x,y,z+1.0,
             x,y+1.0,z+1.0,
+        ];
 
-            // RIGHT
+        vertices.extend_from_slice(&face);
+    }
+
+    fn add_right_face(
+        vertices: &mut Vec<f32>,
+        x: f32,
+        y: f32,
+        z: f32,
+    ) {
+        let face = [
             x+1.0,y+1.0,z+1.0,
             x+1.0,y,z,
             x+1.0,y+1.0,z,
@@ -75,8 +182,18 @@ impl ChunkMesh {
             x+1.0,y,z,
             x+1.0,y+1.0,z+1.0,
             x+1.0,y,z+1.0,
+        ];
 
-            // TOP
+        vertices.extend_from_slice(&face);
+    }
+
+    fn add_top_face(
+        vertices: &mut Vec<f32>,
+        x: f32,
+        y: f32,
+        z: f32,
+    ) {
+        let face = [
             x,y+1.0,z,
             x,y+1.0,z+1.0,
             x+1.0,y+1.0,z+1.0,
@@ -84,8 +201,18 @@ impl ChunkMesh {
             x+1.0,y+1.0,z+1.0,
             x+1.0,y+1.0,z,
             x,y+1.0,z,
+        ];
 
-            // BOTTOM
+        vertices.extend_from_slice(&face);
+    }
+
+    fn add_bottom_face(
+        vertices: &mut Vec<f32>,
+        x: f32,
+        y: f32,
+        z: f32,
+    ) {
+        let face = [
             x,y,z,
             x+1.0,y,z,
             x+1.0,y,z+1.0,
@@ -95,6 +222,6 @@ impl ChunkMesh {
             x,y,z,
         ];
 
-        vertices.extend_from_slice(&cube);
+        vertices.extend_from_slice(&face);
     }
 }

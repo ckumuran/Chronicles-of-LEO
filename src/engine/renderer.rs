ec34d095cff91c15e482c24fb9b8d82c39f29062
kumaran <k31311558@gmail.com>
2026-05-20 16:16:31 +0530

    Update renderer.rs

--- src/engine/renderer.rs
+++ src/engine/renderer.rs
@@ -1,16 +1,20 @@
 use glam::{Mat4, Vec3};
 
 use crate::engine::camera::Camera;
+use crate::engine::chunk_mesh::ChunkMesh;
 use crate::engine::mesh::Mesh;
 use crate::engine::shader::Shader;
+use crate::engine::world::World;
 
 pub struct Renderer {
     shader: Shader,
-    cube: Mesh,
+
+    mesh: Mesh,
 }
 
 impl Renderer {
     pub fn new() -> Self {
+
         let vertex_shader = r#"
             #version 330 core
 
@@ -37,9 +41,27 @@ impl Renderer {
 
             void main()
             {
-                FragColor = vec4(0.8, 0.2, 0.2, 1.0);
+                FragColor = vec4(
+                    0.7,
+                    0.2,
+                    0.2,
+                    1.0
+                );
             }
         "#;
+
+        let world = World::new();
+
+        let chunk =
+            world.get_chunk(0,0,0).unwrap();
+
+        let chunk_mesh =
+            ChunkMesh::build(chunk);
+
+        let mesh =
+            Mesh::from_vertices(
+                &chunk_mesh.vertices
+            );
 
         Self {
             shader: Shader::new(
@@ -47,31 +69,45 @@ impl Renderer {
                 fragment_shader,
             ),
 
-            cube: Mesh::cube(),
+            mesh,
         }
     }
 
-    pub fn render(&self, camera: &Camera) {
+    pub fn render(
+        &self,
+        camera: &Camera,
+    ) {
         self.shader.use_program();
 
-        let model = Mat4::from_translation(
-            Vec3::new(0.0, 0.0, 0.0)
+        let model =
+            Mat4::IDENTITY;
+
+        let view =
+            camera.get_view_matrix();
+
+        let projection =
+            Mat4::perspective_rh_gl(
+                70.0_f32.to_radians(),
+                1280.0 / 720.0,
+                0.1,
+                1000.0,
+            );
+
+        self.shader.set_mat4(
+            "model",
+            &model,
         );
 
-        let view = camera.get_view_matrix();
-
-        let projection = Mat4::perspective_rh_gl(
-            70.0_f32.to_radians(),
-            1280.0 / 720.0,
-            0.1,
-            1000.0,
+        self.shader.set_mat4(
+            "view",
+            &view,
         );
 
-        self.shader.set_mat4("model", &model);
-        self.shader.set_mat4("view", &view);
-        self.shader
-            .set_mat4("projection", &projection);
+        self.shader.set_mat4(
+            "projection",
+            &projection,
+        );
 
-        self.cube.draw();
+        self.mesh.draw();
     }
 }

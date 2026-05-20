eff92058940a9f2be0085eb3fac23197f7d582ab
kumaran <k31311558@gmail.com>
2026-05-20 16:10:09 +0530

    Create mesh.rs

---
+++ src/engine/mesh.rs
@@ -0,0 +1,113 @@
+use std::mem;
+use std::os::raw::c_void;
+
+pub struct Mesh {
+    vao: u32,
+    vbo: u32,
+
+    vertex_count: i32,
+}
+
+impl Mesh {
+    pub fn cube() -> Self {
+        let vertices: [f32; 108] = [
+            // FRONT
+            -0.5,-0.5, 0.5,
+             0.5,-0.5, 0.5,
+             0.5, 0.5, 0.5,
+
+             0.5, 0.5, 0.5,
+            -0.5, 0.5, 0.5,
+            -0.5,-0.5, 0.5,
+
+            // BACK
+            -0.5,-0.5,-0.5,
+            -0.5, 0.5,-0.5,
+             0.5, 0.5,-0.5,
+
+             0.5, 0.5,-0.5,
+             0.5,-0.5,-0.5,
+            -0.5,-0.5,-0.5,
+
+            // LEFT
+            -0.5, 0.5, 0.5,
+            -0.5, 0.5,-0.5,
+            -0.5,-0.5,-0.5,
+
+            -0.5,-0.5,-0.5,
+            -0.5,-0.5, 0.5,
+            -0.5, 0.5, 0.5,
+
+            // RIGHT
+             0.5, 0.5, 0.5,
+             0.5,-0.5,-0.5,
+             0.5, 0.5,-0.5,
+
+             0.5,-0.5,-0.5,
+             0.5, 0.5, 0.5,
+             0.5,-0.5, 0.5,
+
+            // TOP
+            -0.5, 0.5,-0.5,
+            -0.5, 0.5, 0.5,
+             0.5, 0.5, 0.5,
+
+             0.5, 0.5, 0.5,
+             0.5, 0.5,-0.5,
+            -0.5, 0.5,-0.5,
+
+            // BOTTOM
+            -0.5,-0.5,-0.5,
+             0.5,-0.5,-0.5,
+             0.5,-0.5, 0.5,
+
+             0.5,-0.5, 0.5,
+            -0.5,-0.5, 0.5,
+            -0.5,-0.5,-0.5,
+        ];
+
+        let mut vao = 0;
+        let mut vbo = 0;
+
+        unsafe {
+            gl::GenVertexArrays(1, &mut vao);
+            gl::GenBuffers(1, &mut vbo);
+
+            gl::BindVertexArray(vao);
+
+            gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+
+            gl::BufferData(
+                gl::ARRAY_BUFFER,
+                (vertices.len() * mem::size_of::<f32>()) as isize,
+                vertices.as_ptr() as *const c_void,
+                gl::STATIC_DRAW,
+            );
+
+            gl::VertexAttribPointer(
+                0,
+                3,
+                gl::FLOAT,
+                gl::FALSE,
+                (3 * mem::size_of::<f32>()) as i32,
+                std::ptr::null(),
+            );
+
+            gl::EnableVertexAttribArray(0);
+        }
+
+        Self {
+            vao,
+            vbo,
+            vertex_count: 36,
+        }
+    }
+
+    pub fn draw(&self) {
+        unsafe {
+            gl::BindVertexArray(self.vao);
+
+            gl::DrawArrays(gl::TRIANGLES, 0, self.vertex_count);
+        }
+    }
+}

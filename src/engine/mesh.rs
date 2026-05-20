efffbece9218e29104824f74c579783a673e1cfb
kumaran <k31311558@gmail.com>
2026-05-20 16:28:56 +0530

    Update mesh.rs

--- src/engine/mesh.rs
+++ src/engine/mesh.rs
@@ -2,6 +2,7 @@ use std::mem;
 use std::os::raw::c_void;
 
 pub struct Mesh {
+
     vao: u32,
     vbo: u32,
 
@@ -9,47 +10,97 @@ pub struct Mesh {
 }
 
 impl Mesh {
-    pub fn from_vertices(vertices: &[f32]) -> Self {
+
+    pub fn from_vertices(
+        vertices: &[f32]
+    ) -> Self {
+
         let mut vao = 0;
         let mut vbo = 0;
 
         unsafe {
-            gl::GenVertexArrays(1, &mut vao);
-            gl::GenBuffers(1, &mut vbo);
 
-            gl::BindVertexArray(vao);
+            gl::GenVertexArrays(
+                1,
+                &mut vao,
+            );
 
-            gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+            gl::GenBuffers(
+                1,
+                &mut vbo,
+            );
+
+            gl::BindVertexArray(
+                vao
+            );
+
+            gl::BindBuffer(
+                gl::ARRAY_BUFFER,
+                vbo,
+            );
 
             gl::BufferData(
                 gl::ARRAY_BUFFER,
-                (vertices.len() * mem::size_of::<f32>()) as isize,
-                vertices.as_ptr() as *const c_void,
+
+                (vertices.len() *
+                 mem::size_of::<f32>())
+                    as isize,
+
+                vertices.as_ptr()
+                    as *const c_void,
+
                 gl::STATIC_DRAW,
             );
+
+            let stride =
+                5 * mem::size_of::<f32>();
+
+            // POSITION
 
             gl::VertexAttribPointer(
                 0,
                 3,
                 gl::FLOAT,
                 gl::FALSE,
-                (3 * mem::size_of::<f32>()) as i32,
+                stride as i32,
                 std::ptr::null(),
             );
 
             gl::EnableVertexAttribArray(0);
+
+            // UV
+
+            gl::VertexAttribPointer(
+                1,
+                2,
+                gl::FLOAT,
+                gl::FALSE,
+                stride as i32,
+
+                (3 * mem::size_of::<f32>())
+                    as *const c_void,
+            );
+
+            gl::EnableVertexAttribArray(1);
         }
 
         Self {
+
             vao,
             vbo,
-            vertex_count: (vertices.len() / 3) as i32,
+
+            vertex_count:
+                (vertices.len() / 5) as i32,
         }
     }
 
     pub fn draw(&self) {
+
         unsafe {
-            gl::BindVertexArray(self.vao);
+
+            gl::BindVertexArray(
+                self.vao
+            );
 
             gl::DrawArrays(
                 gl::TRIANGLES,

bd3ad331f6b97ceab488a14603536ea5d505c7b5
kumaran <k31311558@gmail.com>
2026-05-20 16:26:10 +0530

    Update world.rs

--- src/engine/world.rs
+++ src/engine/world.rs
@@ -1,3 +1,4 @@
+use crate::engine::block::BlockType;
 use crate::engine::chunk_manager::ChunkManager;
 
 pub struct World {
@@ -10,6 +11,7 @@ impl World {
     pub fn new() -> Self {
 
         Self {
+
             chunk_manager:
                 ChunkManager::new(),
         }
@@ -32,5 +34,51 @@ impl World {
             player_chunk_x,
             player_chunk_z,
         );
+    }
+
+    pub fn set_block(
+        &mut self,
+
+        world_x: i32,
+        world_y: i32,
+        world_z: i32,
+
+        block: BlockType,
+    ) {
+
+        let chunk_x =
+            (world_x as f32 / 16.0).floor() as i32;
+
+        let chunk_z =
+            (world_z as f32 / 16.0).floor() as i32;
+
+        let local_x =
+            ((world_x % 16) + 16) % 16;
+
+        let local_z =
+            ((world_z % 16) + 16) % 16;
+
+        let local_y =
+            world_y;
+
+        if let Some(chunk) =
+            self.chunk_manager
+                .chunks
+                .get_mut(&(chunk_x, 0, chunk_z))
+        {
+
+            if local_y >= 0 &&
+               local_y < 16
+            {
+
+                chunk.set_block(
+                    local_x as usize,
+                    local_y as usize,
+                    local_z as usize,
+
+                    block,
+                );
+            }
+        }
     }
 }

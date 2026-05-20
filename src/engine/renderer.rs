631a176379a40ffbee36b717cee89432faf9c165
kumaran <k31311558@gmail.com>
2026-05-20 16:11:58 +0530

    Update renderer.rs

--- src/engine/renderer.rs
+++ src/engine/renderer.rs
@@ -1,3 +1,6 @@
+use glam::{Mat4, Vec3};
+
+use crate::engine::camera::Camera;
 use crate::engine::mesh::Mesh;
 use crate::engine::shader::Shader;
 
@@ -13,9 +16,17 @@ impl Renderer {
 
             layout (location = 0) in vec3 aPos;
 
+            uniform mat4 model;
+            uniform mat4 view;
+            uniform mat4 projection;
+
             void main()
             {
-                gl_Position = vec4(aPos, 1.0);
+                gl_Position =
+                    projection *
+                    view *
+                    model *
+                    vec4(aPos, 1.0);
             }
         "#;
 
@@ -31,14 +42,35 @@ impl Renderer {
         "#;
 
         Self {
-            shader: Shader::new(vertex_shader, fragment_shader),
+            shader: Shader::new(
+                vertex_shader,
+                fragment_shader,
+            ),
 
             cube: Mesh::cube(),
         }
     }
 
-    pub fn render(&self) {
+    pub fn render(&self, camera: &Camera) {
         self.shader.use_program();
+
+        let model = Mat4::from_translation(
+            Vec3::new(0.0, 0.0, 0.0)
+        );
+
+        let view = camera.get_view_matrix();
+
+        let projection = Mat4::perspective_rh_gl(
+            70.0_f32.to_radians(),
+            1280.0 / 720.0,
+            0.1,
+            1000.0,
+        );
+
+        self.shader.set_mat4("model", &model);
+        self.shader.set_mat4("view", &view);
+        self.shader
+            .set_mat4("projection", &projection);
 
         self.cube.draw();
     }
